51948ec8a53d75347f6bbe03c12575643fbf1b70
Yurii.B <yr.bogdan@gmail.com>
2015-04-01 21:01:17 +0300

    Initial commit

---
+++ AlbumPicker/AlbumPicker-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  AlbumPicker-Bridging-Header.h
+//  AlbumPicker
+//
+//  Created by iMAC i7 on 4/1/15.
+//  Copyright (c) 2015 YuriiMobile. All rights reserved.
+//
+
+#import "iCarousel.h"
+#import "UIImageView+WebCache.h"
